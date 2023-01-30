434498a1040404251a31d5b0b1ac2e3d66ca0794
abdulman7823 <abdulman4401@gmail.com>
2023-01-30 23:20:56 +0500

    app.js-edit

--- app.js
+++ app.js
@@ -26,8 +26,9 @@ dotenv.config();
 
 app.use(logger("dev"));
 app.use('/api/stripe/webhook', express.raw({ type: '/' }));
-app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
