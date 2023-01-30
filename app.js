ad9c5523a18e287f8c2d09c752ffd53954a543ee
abdulman7823 <abdulman4401@gmail.com>
2023-01-30 23:30:24 +0500

    remove-express.json

--- app.js
+++ app.js
@@ -26,7 +26,6 @@ dotenv.config();
 
 app.use(logger("dev"));
 app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
 app.use('/api/stripe/webhook', express.raw({ type: '/' }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
